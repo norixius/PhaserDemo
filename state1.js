23d1dc513b203d40c76729dd0e8c4b4f076b67a1
Paulo Kreft <paulo.kreft@gmail.com>
2020-01-09 10:00:58 -0300

    Estudado em Casa dia 09/01/2020

--- state1.js
+++ state1.js
@@ -1,10 +1,20 @@
 demo.state1 = function(){};
 demo.state1.prototype = {
-    preload: function(){},
+    preload: function(){
+      game.load.tilemap('field', 'assets/tilemaps/field.json', null, Phaser.Tilemap.TILED_JSON);
+      game.load.image('grassTiles', 'assets/tilemaps/grassTiles.png');
+      game.load.image('rockTiles', 'assets/tilemaps/rockTiles.png');
+    },
     create: function(){
       game.stage.backgroundColor = '#DDDDDD';
-
       addChangeStateEventListeners();
+
+      let map = game.add.tilemap('field');
+      map.addTilesetImage('grassTiles');
+      map.addTilesetImage('rockTiles');
+
+      let grass = map.createLayer('grass');
+      let rocks = map.createLayer('rocks');
     },
     update: function(){}
 };
